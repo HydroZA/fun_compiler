ea4438e721314ddadc759bd66d4eec309000571b
HydroZA <jmslegge0@gmail.com>
2020-12-19 08:50:23 +0200

    Initial Commit

---
+++ fun_compiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fun_compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
